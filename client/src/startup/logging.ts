b204aa12efdb951d1b29c345fd2557a5ed807c48
Andrew Courtice <andrewcourtice@users.noreply.github.com>
2020-03-24 14:29:28 +1000

    fix(logging): fixed sentry logging (temporarily)

--- client/src/startup/logging.ts
+++ client/src/startup/logging.ts
@@ -4,15 +4,19 @@ import {
     init
 } from '@sentry/browser';
 
-import {
-    Vue as SentryVue
-} from '@sentry/integrations';
+// import {
+//     Vue as SentryVue
+// } from '@sentry/integrations';
 
 export default function initialiseLogging() {
     init({
+        enabled: true,
         dsn: process.env.SENTRY_DSN,
-        integrations: [
-            new SentryVue({ Vue })
-        ]
+        // integrations: [
+        //     new SentryVue({
+        //         Vue,
+        //         attachProps: true 
+        //     })
+        // ]
     });
 }
