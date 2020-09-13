30d4bf8a47c4e0c8dddb6539f6fdb0b339bebf00
Andrew Courtice <andrewcourtice@users.noreply.github.com>
2020-09-13 13:25:35 +1000

    enhancement(components): added tooltip

--- packages/components/src/directives/index.ts
+++ packages/components/src/directives/index.ts
@@ -1,9 +1,11 @@
 import focus from './focus';
 import meta from './meta';
+import tooltip from './tooltip';
 import visible from './visible';
 
 export default {
     focus,
     meta,
+    tooltip,
     visible
 };
