73f545ad6d6ebbdada68c7e552fe69c6052d2736
Andrew Courtice <andrewcourtice@users.noreply.github.com>
2020-09-23 18:28:02 +1000

    Update config.js

--- client/build/_base/config.js
+++ client/build/_base/config.js
@@ -102,7 +102,7 @@ export default {
                 appShortName: 'Ocula',
                 appDescription: 'The open-source, progressive weather app',
                 developerName: 'Andrew Courtice',
-                display: 'minimal-ui',
+                display: 'standalone',
                 background: '#FFFFFF',
                 theme_color: '#FFFFFF',
                 appleStatusBarStyle: 'default',
