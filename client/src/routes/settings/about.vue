65cb64faac267ef9ed8f9edbd043fe866ce7a461
Andrew Courtice <andrewcourtice@users.noreply.github.com>
2020-10-07 10:25:44 +1000

    Release 1.0.1 (#141)
    * enhancement(settings): updated settings styles and added sponsor
    
    * fix(package): fixed root package.json
    
    * v1.0.1

--- client/src/routes/settings/about.vue
+++ client/src/routes/settings/about.vue
@@ -1,33 +1,42 @@
 <template>
     <settings-layout class="route settings-about" title="About" :back-route="backRoute">
-        <div class="padding__horizontal--large">
-            <div class="text--centre">
+        <div class="settings-about__body">
+            <div class="settings-about__header">
                 <img class="settings-about__logo" :src="logo" alt="Ocula">
-                <h1>{{ manifest.title }}</h1>
-                <div>{{ manifest.description }}</div>
+                <h1 class="settings-about__title">{{ manifest.title }}</h1>
+                <div class="settings-about__subtitle">{{ manifest.description }}</div>
             </div>
-            <div class="settings-about__details-grid margin__top--large">
-                <strong>Version</strong>
-                <div>{{ manifest.version }}</div>
-                <strong>Author</strong>
-                <div>{{ manifest.author }}</div>
-                <strong>Licence</strong>
-                <div>{{ manifest.license }}</div>
-                <strong>Source</strong>
-                <a :href="manifest.repository" target="_blank">Github</a>
-            </div>
-            <div class="settings-about__details-grid margin__top--large">
-                <strong>Forecast Data</strong>
-                <a href="https://openweathermap.org" target="_blank">Open Weather Maps</a>
-                <strong>Maps/Geocoding</strong>
-                <a href="https://www.mapbox.com" target="_blank">Mapbox</a>
-                <strong>Map Tiles</strong>
-                <a href="https://www.rainviewer.com" target="_blank">RainViewer</a>
-                <strong>Logo Design</strong>
-                <a href="https://github.com/ethanroxburgh" target="_blank">Ethan Roxburgh</a>
-                <strong>Icons</strong>
-                <a href="https://remixicon.com/" target="_blank">Remix Icons</a>
-            </div>
+            <block class="settings-about__block" title="Details">
+                <div class="settings-about__details-grid">
+                    <strong>Version</strong>
+                    <div>{{ manifest.version }}</div>
+                    <strong>Author</strong>
+                    <div>{{ manifest.author }}</div>
+                    <strong>Licence</strong>
+                    <div>{{ manifest.license }}</div>
+                    <strong>Source</strong>
+                    <a :href="manifest.repository" target="_blank">Github</a>
+                </div>
+            </block>
+            <block class="settings-about__block" title="Attribution">
+                <div class="settings-about__details-grid">
+                    <strong>Forecast Data</strong>
+                    <a href="https://openweathermap.org" target="_blank">Open Weather Maps</a>
+                    <strong>Tide Data</strong>
+                    <a href="https://www.worldtides.info" target="_blank">World Tides</a>
+                    <strong>Maps/Geocoding</strong>
+                    <a href="https://www.mapbox.com" target="_blank">Mapbox</a>
+                    <strong>Radar Imagery</strong>
+                    <a href="https://www.rainviewer.com" target="_blank">RainViewer</a>
+                    <strong>Logo Design</strong>
+                    <a href="https://github.com/ethanroxburgh" target="_blank">Ethan Roxburgh</a>
+                    <strong>Icons</strong>
+                    <a href="https://remixicon.com" target="_blank">Remix Icons</a>
+                </div>
+            </block>
+            <block class="settings-about__block" title="Sponsors">
+                <div>Kerry Tarrant</div>
+            </block>
             <div class="margin__top--large text--centre">
                 <small class="text--meta">Psalm 147:8</small>
             </div>
@@ -70,8 +79,33 @@ export default defineComponent({
 
 <style lang="scss">
 
+    .settings-about__body {
+        padding: 0 var(--spacing__small);
+    }
+
+    .settings-about__header {
+        text-align: center;
+        margin-bottom: var(--spacing__large);
+    }
+
+    .settings-about__title {
+        text-transform: uppercase;
+        letter-spacing: 0.2em;
+    }
+
     .settings-about__logo {
         width: 96px;
+    }
+
+    .settings-about__block {
+        
+        &:not(:last-of-type) {
+            margin-bottom: var(--spacing__small);
+        }
+
+        & .block__title {
+            color: var(--font__colour--meta);
+        }
     }
 
     .settings-about__details-grid {
